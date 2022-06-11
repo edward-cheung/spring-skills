4c3645bcf530ecdd78730d53a3de74a66368f598
zhangyi01 <zhangyi01@sinoiov.com>
2022-06-11 16:38:34 +0800

    feat: spring cloud

---
+++ cloud-ali/app-consumer/src/test/java/cn/edcheung/cloudali/appconsumer/AppConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edcheung.cloudali.appconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
